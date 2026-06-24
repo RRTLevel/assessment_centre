e884319c8c76eabb1edaa34998a17afb7c4fef4d
Daniel <daniel@familychapman.net>
2026-06-24 11:11:35 +0100

    fix for the wrong indentation on the forms page

--- app/app_src/forms.py
+++ app/app_src/forms.py
@@ -207,10 +207,10 @@ class QuestionForm(forms.Form):
             "class": "select",
         })
     )
-        labels = {
-            "text": "Enter your Question:",
-            "category": "",
-        }
+    labels = {
+        "text": "Enter your Question:",
+        "category": "",
+    }
 
 
 class InterviewResponseForm(forms.ModelForm):
